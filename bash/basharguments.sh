a7917816f988dbd01d4b9b0590ceb9495451ab7d
Ravi kiran <bhagsukraj@gmail.com>
2017-10-12 19:20:54 +0530

    update

---
+++ bash/basharguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#passing arguments to the bash script
+
+var1=$#
+var2=$#
+var3=$#
+
+echo var1 var2 var3
+
+exit
--- bash/bashsh1.sh
+++ bash/bashsh1.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+#this is a comment
+echo "Hello World!"
+
+var1="India is a great country"
+var2="Fine country"
+
