a5f0bad1ca9ae7ce819178d009b173173fd39126
Ravi kiran <bhagsukraj@gmail.com>
2017-10-13 18:45:02 +0530

    update

--- bash/basharguments.sh
+++ bash/basharguments.sh
@@ -2,10 +2,18 @@
 
 #passing arguments to the bash script
 
-var1=$#
-var2=$#
-var3=$#
+var1=$1
+var2=$2
+var3=$3
 
-echo var1 var2 var3
+echo $var1 $var2 $var3
+
+if [ $var1 = "ravi" ] ; then
+  echo "awesome this thing works"
+elif [ $var1 = "kiran" ] ; then
+  echo "first elif"
+else
+  echo "else statement"
+fi
 
 exit
