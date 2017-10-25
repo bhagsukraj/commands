9689e3d7f79b2b10bfc125556b143000c58a6418
Ravi kiran <bhagsukraj@gmail.com>
2017-10-25 19:54:54 +0530

    update

--- python sentdex/1pythonhelloworld.py
+++ python sentdex/1pythonhelloworld.py
@@ -1,3 +1,29 @@
+#!python3
+'''
+comments
+'''
+import sys
+import time
 print ('Hello world')
-print ('This is a perfect place to learn python')
-print ('Wonderful india')
+print (sys.version)
+print ('HI'+str(5))
+print(12/5)
+print(13/5)
+print(12*3)
+#unpacking variables
+var = 1
+print('variable',var)
+x,y = (3,5)
+print('x','y',x,y)
+#while loop
+i = 1
+while(i<5):
+    print('Hello')
+    i+=1
+#for loop
+list1 = [ 1 , 100 ,200 , 300, 400]
+for i in list1 :
+    print (i)
+#range function
+for i in range(1,10):
+    print('range',i)
