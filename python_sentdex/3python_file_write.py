6e6f894d7f8496e9609817e9bd878d0284308060
Ravi kiran <bhagsukraj@gmail.com>
2017-10-31 18:52:07 +0530

    update

---
+++ python_sentdex/3python_file_write.py
@@ -0,0 +1,7 @@
+#!python3
+
+text = 'This is the third file'
+
+saveFile = open('exfile1.txt','w')
+saveFile.write(text)
+saveFile.close()
---
+++ python_sentdex/4python_file_append.py
@@ -0,0 +1,8 @@
+#!python3
+
+atext = '\nThis is hyperloop'
+
+afile = open('exfile1.txt','a')
+afile.write(atext)
+
+afile.close()
