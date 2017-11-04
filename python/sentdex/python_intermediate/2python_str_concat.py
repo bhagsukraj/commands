2bf066a1dc80723d9edba3562ecb43e56a3748a1
Ravi kiran <bhagsukraj@gmail.com>
2017-11-04 19:16:26 +0530

    update

--- python/sentdex/python_intermediate/2python_str_concat.py
+++ python/sentdex/python_intermediate/2python_str_concat.py
@@ -1,6 +1,8 @@
 #!python3
 list1 = ['apple','mango','grape','dragon fruit']
 
+print(', '.join(list1))
+
 for i in list1:
     print('Fruit name is : ' + i)
     print(' '.join(['Fruit name is :',i]))
