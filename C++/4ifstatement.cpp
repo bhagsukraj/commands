0f92dc7fdfa9ce0994210ee289f1a0a8f2c44892
Ravi kiran <bhagsukraj@gmail.com>
2017-11-18 22:39:44 +0530

    commit

---
+++ C++/4ifstatement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  int a = 1;
+  int b = 2;
+
+  if(a>b){
+    cout << "a is greater than b";
+  }else{
+    cout << "a is lesser than b";
+  }
+  return 0;
+}
