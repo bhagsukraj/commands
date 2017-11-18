fb1631fced52ace0ee56cab3a033dfa40b6ca600
Ravi kiran <bhagsukraj@gmail.com>
2017-11-18 19:28:06 +0530

    update

---
+++ C++/3calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  int sum;
+
+  cout << "Enter a number boss! \n";
+  cin  >> a;
+  cout << "Enter the second number!\n";
+  cin  >> b;
+
+  sum = a + b;
+  cout << "The sum of numbers is " << sum << endl ;
+
+  return 0;
+}
