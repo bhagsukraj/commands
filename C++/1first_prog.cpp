8ad7e6f449b74a1f6938d75ef6c2270d693b3676
Ravi kiran <bhagsukraj@gmail.com>
2017-11-18 18:57:56 +0530

    update

---
+++ C++/1first_prog.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+  cout << "Boy \n" ;
+  cout << "I love you" ;
+  cout << "\nThis is new line" << endl ;
+  cout << "Begin of another new line" ;
+  return 0;
+}
