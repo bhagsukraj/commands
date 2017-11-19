e5f44c74abe8f93be2188c2dad5f0c3182a46f79
Ravi kiran <bhagsukraj@gmail.com>
2017-11-19 13:14:22 +0530

    update

--- C++/5functions.cpp
+++ C++/5functions.cpp
@@ -5,13 +5,19 @@
 using namespace std;
 
 void printSomething();
+int printCrap(int x , int y);
 
 int main()
 {
   printSomething();
+  cout << "The return value of sum is " << printCrap(3,10) << endl;
   return 0;
 }
 
 void printSomething(){
   cout << "I am writing my first c function";
+}
+
+int printCrap(int x , int y){
+  return x+y;
 }
---
+++ C++/6classesAndObjects.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+class myFirstClass{
+  public:
+    int var1;
+    void simpleFunc(){
+      cout << "This is a first class method simpleFunc";
+      var1 = 246;
+      cout << "The public variable is " << var1 ;
+    };
+
+    int var2=100;
+    void simpleFunc2(int x , int y){
+      cout << "\nThe sum on first class is " << x+y << endl ;
+      cout << "\nThe second variable is " << var2 ;
+    }
+};
+
+int main(){
+  myFirstClass myFirstObject ;
+  myFirstObject.simpleFunc();
+  myFirstObject.simpleFunc2(3,4);
+  return 0;
+}
