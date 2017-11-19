b82c350ad3d6faf94754507471650feac0310c7d
Ravi kiran <bhagsukraj@gmail.com>
2017-11-19 11:45:45 +0530

    update

---
+++ C++/5functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+void printSomething();
+
+int main()
+{
+  printSomething();
+  return 0;
+}
+
+void printSomething(){
+  cout << "I am writing my first c function";
+}
