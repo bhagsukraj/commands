95b63b281a10d49b6d3a386c407d643cbc3f7c12
Ravi kiran <bhagsukraj@gmail.com>
2017-12-30 17:37:06 +0530

    update

---
+++ C++/1helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+  cout << "Hello world"  << "\tperfect world"
+    << "\nwonderful world" << endl;
+  return 0;
+};
