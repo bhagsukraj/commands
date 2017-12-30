00c724f4c1bac1a3e34e8b62d612f2206af2af27
Ravi kiran <bhagsukraj@gmail.com>
2017-12-30 17:54:06 +0530

    update

---
+++ C++/2variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+  int x = 1;
+  int y = 24;
+  cout << "Variables \n";
+  cout << x << "\n" ;
+  cout << x + y ;
+  return 0;
+};
