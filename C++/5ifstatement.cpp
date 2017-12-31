56ade8716ba3c1de37bbdfd99e95e69cd2384b9e
Ravi kiran <bhagsukraj@gmail.com>
2017-12-31 11:28:24 +0530

    update

--- C++/5ifstatement.cpp
+++ C++/5ifstatement.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main(){
   int a , b ;
-  cout << "If statement";
+  cout << "If statement \n";
 
   a = 10 , b = 10 ;
 
   if (a == b){
-    cout << " a is equal to b";
+    cout << "a is equal to b";
   };
+
+  if (a != b){
+    cout << "a is not equal to b";
+  }
 }
