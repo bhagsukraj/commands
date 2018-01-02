d8610191dec0c3d1f1c1c59199e6ad66da50dfb6
Ravi kiran <bhagsukraj@gmail.com>
2018-01-02 10:29:42 +0530

    update

---
+++ C++/10_1_class.h
@@ -0,0 +1,11 @@
+#include <cstdlib>
+
+using namespace std;
+
+class sample{
+  public:
+    int meth1(){
+      cout << "This is printed out of a header file" ;
+      return 0;
+    };
+};
---
+++ C++/10_2_class_main.cpp
@@ -0,0 +1,10 @@
+#include "10_1_class.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+  sample sample_bo ;
+  sample_bo.meth1();
+};
