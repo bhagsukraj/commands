9c7964899f5d82fde08e0ba5b345abd4cf208087
Ravi kiran <bhagsukraj@gmail.com>
2018-01-05 07:42:02 +0530

    update

--- C++/thenewboston/15switch.cpp
+++ C++/thenewboston/15switch.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main(){
-  
+  int age = 18;
+
+  switch(age){
+    case 10:
+      cout << "Young kid";
+      break;
+    case 16:
+      cout << "Teen";
+      break;
+    case 18:
+      cout << "Adult";
+      break;
+  }
 }
