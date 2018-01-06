12dc8eed398b9f8ebe12f23b4af2e3aa015e0d84
Ravi kiran <bhagsukraj@gmail.com>
2018-01-06 16:05:28 +0530

    update

--- C++/thenewboston/22arrays.cpp
+++ C++/thenewboston/22arrays.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(){
   int array1[5] = {1,22,34,43,56};
+  int array2[9];
 
   cout << array1[2] << endl;
-}
+
+  for(int x = 0 ; x < 9 ; x++){
+    array2[x] = x + 5 ;
+    cout << x << "---------" << array2[x] << endl;
+  };
+
+};
