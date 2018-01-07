5d4fb92c35843d4bb714ea5bd6409cb88090ea1e
Ravi kiran <bhagsukraj@gmail.com>
2018-01-07 08:08:57 +0530

    update

--- C++/thenewboston/29constants.cpp
+++ C++/thenewboston/29constants.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void printNum(int a , const int b){
+  a = 10;
+
+  cout << a << endl << b << endl ;
+}
+
 int main(){
   const int i = 10;
 
   cout << i << endl;
+
+  printNum(10,100);
 }
