401a53d76efabe42d16606a923048561bb549f17
Ravi kiran <bhagsukraj@gmail.com>
2018-01-07 07:05:42 +0530

    update

---
+++ C++/thenewboston/25pointers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "pointers" << endl;
+  int a = 100;
+  cout << &a << endl ;
+// * before a variable indicates pointer variable.
+  int *fishPointer;
+  fishPointer = &a;
+  cout << fishPointer << endl ;
+}
---
+++ C++/thenewboston/26passbyrefwithpointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void passByValue(int x);
+void passByRef(int *y);
+
+int main(){
+  int x = 100;
+  int y = 200;
+
+  passByValue(x);
+  passByRef(&y);
+
+  cout << "x" << x << endl ;
+  cout << "y" << y << endl ;
+}
+
+void passByValue(int x){
+  x = 50;
+}
+
+void passByRef(int *y){
+  *y = 30;
+}
