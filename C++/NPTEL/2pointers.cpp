52aefd2cf268fcd8bd17d6b797b073f350d0bd80
Ravi kiran <bhagsukraj@gmail.com>
2018-01-14 13:50:55 +0530

    update

---
+++ C++/NPTEL/2pointers.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int *ip;
+  int i;
+
+  ip = &i ;
+  printf("The value of pointer is %p \n", ip);
+  printf("The value of pointer is %p \n", &i);
+  // The value of pointee is
+  printf("The value of pointer is %d   ", *ip);
+  return 0;
+}
