113b83c1c3bb4411c4819ae8a8367dfa961d7737
Ravi kiran <bhagsukraj@gmail.com>
2018-01-15 09:47:01 +0530

    update

---
+++ C++/NPTEL/10referenceandpointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a=10 , &b = a;
+
+  cout << "a = " << a << " , b = " << b << endl;
+  cout << "&a = " << &a << endl << "&b = " << &b << endl;
+
+  a = 20;
+  cout << "b = " << b << endl;
+
+  a = 30;
+  cout << "b = " << b;
+}
