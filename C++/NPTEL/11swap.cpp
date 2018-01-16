55ff9584a6ce911b6f38d9a42976e94409521234
Ravi kiran <bhagsukraj@gmail.com>
2018-01-16 07:45:42 +0530

    update

--- C++/NPTEL/11swap.cpp
+++ C++/NPTEL/11swap.cpp
@@ -6,12 +6,12 @@ void swap(int *x , int *y);
 
 int main(){
   int x = 10 , y = 20 ;
-  swap(&x , &y);
+  swap(&x , &y) ;
 };
 
 void swap(int *x , int *y){
   int t = *x ;
-  
+
   cout << "The pointer of x is " << x << endl;
   cout << "The pointer of y is " << y << endl;
   *x = *y ;
