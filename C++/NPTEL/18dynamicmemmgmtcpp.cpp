50b07521b5f9b9b9035e7b2a7a89a2ec065e990f
Ravi kiran <bhagsukraj@gmail.com>
2018-01-21 18:17:19 +0530

    update

--- C++/NPTEL/18dynamicmemmgmtcpp.cpp
+++ C++/NPTEL/18dynamicmemmgmtcpp.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main(){
   int *p = new int(10);
+  int *a = new int[3];
 
   cout << "The value of pointer p is " << p << endl;
   cout << "The value is " << *p << endl;
+
+  a[0] = 10;
+  a[1] = 20;
+  a[2] = 30;
+
+  cout << "The value of pointer a is "<< a << endl;
+  cout << "The value of a(0) "<< a[0] <<endl;
+  delete p;
+  delete a;
+  return 0;
 }
