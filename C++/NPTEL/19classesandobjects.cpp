53c5b455be76e30a7fb995a8879c27065a83faee
Ravi kiran <bhagsukraj@gmail.com>
2018-01-23 21:38:35 +0530

    update

--- C++/NPTEL/19classesandobjects.cpp
+++ C++/NPTEL/19classesandobjects.cpp
@@ -4,6 +4,11 @@ using namespace std;
 class class1{
   public:
     double val1 , val2 ;
+    double fetch_val(double val3 , double val4){
+      val3 = this->val1;
+      val4 = this->val2;
+      return val3+val4;
+    }
 };
 
 
@@ -11,6 +16,9 @@ int main(){
   class1 bo = { 1.2345 , 2.3456 };
 
   cout << bo.val1 << "," << bo.val2 << endl;
+
+  double val1 = bo.fetch_val(3,4);
+  cout << "This pointer " << val1;
 
   return 0;
 }
