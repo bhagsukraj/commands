92d6854441d8f86c4d4f8c82bdfc3c5603e7108f
Ravi kiran <bhagsukraj@gmail.com>
2018-01-25 09:22:10 +0530

    update

---
+++ C++/NPTEL/21dynamicclassinstance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class class1{
+  private:
+    int var1;
+  public:
+    class1():var1(10){
+      cout <<"This class constructor var1 value is " << var1 << endl;
+    };
+    void meth1(){
+      cout << "printing using pointer";
+    };
+};
+
+int main(){
+  class1* bo = new class1();
+  bo->meth1();
+  delete bo;
+  return 0;
+}
