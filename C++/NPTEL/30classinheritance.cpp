ec31a1e0930787f0a32d4f5b8822b25e6bcc8082
Ravi kiran <bhagsukraj@gmail.com>
2018-01-28 07:20:54 +0530

    update

--- C++/NPTEL/30classinheritance.cpp
+++ C++/NPTEL/30classinheritance.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class base{
+  private:
+    int i , j;
+  public:
+    void meth1(){
+      cout <<"Base class" << endl;
+    };
+};
+
+class derived : public base{
+public:
+  void meth1(){
+    cout <<"Derived class method" << endl;
+  };
+};
+
 int main(){
-  
-
-
+  derived obj1 ;
+  obj1.meth1();
+  // obj1.meth2();
   return 0;
 };
