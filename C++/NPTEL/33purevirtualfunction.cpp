b3c7d664abff7d116fd134733c0a3e3f02334522
Ravi kiran <bhagsukraj@gmail.com>
2018-02-01 15:59:01 +0530

    update

---
+++ C++/NPTEL/33purevirtualfunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+class class1{
+  public:
+    // = 0 means pure virtual function.
+    virtual void meth1() = 0;
+};
+
+class class2 : public class1{
+  public:
+    void meth1(){
+      cout << "Concrete class" ;
+    };
+};
+
+int main(){
+  class2 obj;
+  obj.meth1();
+  return 0;
+}
