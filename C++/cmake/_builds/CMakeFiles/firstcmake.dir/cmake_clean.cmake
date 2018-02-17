c13b591b43906e3c55cfb705b0eb397f7b0aa38f
Ravi kiran <bhagsukraj@gmail.com>
2018-02-17 16:45:46 +0530

    update

---
+++ C++/cmake/_builds/CMakeFiles/firstcmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstcmake.dir/firstcmake.cpp.o"
+  "firstcmake.pdb"
+  "firstcmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstcmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
