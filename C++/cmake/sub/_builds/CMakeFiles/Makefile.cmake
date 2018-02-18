1b90e214b48d975fa3235e779fd4d637d46a5abd
Ravi kiran <bhagsukraj@gmail.com>
2018-02-18 16:20:04 +0530

    update

--- C++/cmake/sub/_builds/CMakeFiles/Makefile.cmake
+++ C++/cmake/sub/_builds/CMakeFiles/Makefile.cmake
@@ -11,7 +11,6 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.5.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.5.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.5.1/CMakeSystem.cmake"
-  "../sub/CMakeLists.txt"
   "/usr/share/cmake-3.5/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.5/Modules/CMakeCXXInformation.cmake"
   "/usr/share/cmake-3.5/Modules/CMakeCommonLanguageInclude.cmake"
@@ -38,11 +37,9 @@ set(CMAKE_MAKEFILE_OUTPUTS
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/CMakeDirectoryInformation.cmake"
-  "sub/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/firstcmake.dir/DependInfo.cmake"
-  "sub/CMakeFiles/sub.dir/DependInfo.cmake"
+  "CMakeFiles/sub.dir/DependInfo.cmake"
   )
---
+++ C++/cmake/sub/sub.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+  cout << "Perfect India" << endl;
+  cout << "Sub folder" << endl;
+  cout << "I love my india" << endl;
+}
