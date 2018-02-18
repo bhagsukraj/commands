ee121954b65c7bdd5a94f6876db7231addaf1b9f
Ravi kiran <bhagsukraj@gmail.com>
2018-02-18 12:12:47 +0530

    update

--- C++/cmake/firstcmake.cpp
+++ C++/cmake/firstcmake.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 
 int main(){
-  cout << "Hello world" ;
+  cout << "Hello world" << endl;
+  cout << "Perfect world" << endl;
+  cout << "This is the third line" << endl;
 };
