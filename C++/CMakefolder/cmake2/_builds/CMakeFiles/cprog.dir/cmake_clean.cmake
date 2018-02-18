1656b9cbec75b9974377cb58ff94a32f94e94866
Ravi kiran <bhagsukraj@gmail.com>
2018-02-18 17:19:59 +0530

    update

---
+++ C++/CMakefolder/cmake2/_builds/CMakeFiles/cprog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cprog.dir/cprog.cpp.o"
+  "cprog.pdb"
+  "cprog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cprog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
