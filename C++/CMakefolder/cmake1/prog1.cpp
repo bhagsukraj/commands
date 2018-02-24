f3b4739fb8197d57f238c48ff8420a2f8780c163
Ravi kiran <bhagsukraj@gmail.com>
2018-02-24 19:44:43 +0530

    update

---
+++ C++/CMakefolder/cmake1/prog1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello India" << endl;
+  cout << "Hello world" << endl;
+}
--- C++/CMakefolder/cmake1/sub/sub.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main(){
-  cout << "Perfect India" << endl;
-  cout << "Sub folder" << endl;
-  cout << "I love my india" << endl;
-}
