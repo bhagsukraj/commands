736645bfb5664fe61dd33839f436d35fc20e1e2b
Ravi kiran <bhagsukraj@gmail.com>
2018-03-03 14:55:55 +0530

    update

--- C++/CMakefolder/cmake3/foo/_builds/CMakeFiles/Makefile.cmake
+++ C++/CMakefolder/cmake3/foo/_builds/CMakeFiles/Makefile.cmake
@@ -41,5 +41,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/hello.dir/DependInfo.cmake"
+  "CMakeFiles/foo.dir/DependInfo.cmake"
   )
