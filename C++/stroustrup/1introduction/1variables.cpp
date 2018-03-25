d2e12ec03ec7ff9274e7da8d080b90d3c07d3a44
Ravi kiran <bhagsukraj@gmail.com>
2018-03-25 15:33:31 +0530

    update

--- C++/stroustrup/1introduction/1variables.cpp
+++ C++/stroustrup/1introduction/1variables.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
+  double d = 2.2 ;
+  int i = 7;
+  d = d + i;
+  i = d * i;
+  cout<<"The value of d is :" << d << endl;
+  cout<<"The value of i is :" << i << endl;
+
+  string str1 ;
+  // cin>>str1 ;
+  // cout<< "The value of str1 i :" << str1;
+
+  char c1;
+
+  cin >> c1 ;
+  switch(c1){
+    case 'y':
+      cout<<"The value is y" ;
+      return true;
+    case 'x':
+      cout<<"The value is x" ;
+      return true;
+    default:
+      cout<<"The value is infinity" << endl;
+      return false;
+  };
+
+
   return 0;
 }
---
+++ C++/stroustrup/1introduction/2namespace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+namespace name1{
+  int i = 20 ;
+  void prin(int i){
+    cout<< "Print name1: " << i << endl;
+  }
+}
+
+int main(){
+
+  cout<< name1::i << endl;
+  name1::prin( 20 );
+
+  return 0;
+}
